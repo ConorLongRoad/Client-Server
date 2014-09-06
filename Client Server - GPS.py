b1b5eb51ec4de9ae821d5783f520d49e91da7923
ConorLongRoad <30541@longroad.ac.uk>
2014-09-06 23:40:16 +0100

    Client Server
    This was only shown off for a short period of time, but it is extremely
    next-gen coding for using the internet for global tracking, etc. This is
    only a small part of it, and hopefully I won't have to do any GPS
    tracking for my later projects

---
+++ Client Server - GPS.py
@@ -0,0 +1,13 @@
+import urllib.parse,urllib.request, json
+
+def address_to_gps(address): 
+     parameters = {'address': address,'sensor': 'false' } 
+     parameters = urllib.parse.urlencode(parameters) 
+     url = "http://maps.googleapis.com/maps/api/geocode/json?" 
+     url = url + parameters 
+     rawreply = urllib.request.urlopen(url).read() 
+     reply = json.loads(rawreply.decode("utf-8")) 
+     gps = reply['results'][0]['geometry']['location'] 
+     return gps
+    
+print(address_to_gps("Long Road, Cambridge, UK")) 
